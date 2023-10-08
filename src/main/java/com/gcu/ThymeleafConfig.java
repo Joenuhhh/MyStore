3ef4ab347464fd2fab3e05167570e34be55b6cd8
Joenuhhh <79020295+Joenuhhh@users.noreply.github.com>
2023-10-07 18:30:45 -0700

    Your commit message here

--- src/main/java/com/gcu/ThymeleafConfig.java
+++ src/main/java/com/gcu/ThymeleafConfig.java
@@ -5,14 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 public class ThymeleafConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ThymeleafConfig.class);
 
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
+        LOGGER.info("Initializing Thymeleaf Template Resolver");
+        
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setPrefix("classpath:templates/");
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode(TemplateMode.HTML);
         templateResolver.setCharacterEncoding("UTF-8");
